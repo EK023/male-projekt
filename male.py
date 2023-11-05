258c1fd1161afe91082e5062dc8087bacbebce03
EK023 <144771846+EK023@users.noreply.github.com>
2023-11-05 15:34:31 +0200

    lõplik vankri käigu leidmine

--- male.py
+++ male.py
@@ -8,11 +8,11 @@ pygame.init()
 
 #ettur(pawn),vanker(rook),ratsu(horse),oda(bishop),kuningas,lipp(queen)//
 algseis= [['v','r','o','l','k','o','r','v'],
-          ['','e','e','e','e','e','e','e'],		#väikse tähega on mustad ja esitähega on eristatavad nupud
-          ['','','','','','','',''],
-          ['','','','','','','',''],
-          ['','','','','','','',''],
-          ['','','','','','','',''],
+          ['e','e','e','e','e','e','e','e'],		#väikse tähega on mustad ja esitähega on eristatavad nupud
+          [' ',' ',' ',' ',' ',' ',' ',' '],
+          [' ',' ',' ',' ',' ',' ',' ',' '],
+          [' ',' ',' ',' ',' ',' ',' ',' '],
+          [' ',' ',' ',' ',' ',' ',' ',' '],
           ['E','E','E','E','E','E','E','E'],		#suure tähega valged
           ['V','R','O','L','K','O','R','V']]
 valged=['V','R','O','L','K','E']
@@ -27,19 +27,18 @@ def omad(värv):
         return valged
     else:
         return mustad
-'''def rea_kontroll(seis,vankri_pos,värv, kumb, märk):	#kumb võtab sisendiks kas 'Veerg' või 'Rida' 
-    rida=vankri_pos[0]
-    veerg=vankri_pos[1]
-    võimalused=[]
-    for i in range(rida+1,8):
-        if seis[i][veerg] in vaenlased(värv):
-            võimalused.append([i,veerg])
-            return võimalused
-        if seis[i][veerg] in omad(värv):
-            return võimalused
-        if seis[i][veerg] == '':
-            võimalused.append([i,veerg])'''		#seda ära pane tähele :D
-vankri_käigud(seis,vankri_pos,värv) 		#värvi on tegelikult võimalik leida ka nupu positsioonist(kas ta on väike või suur täht)
+'''rea_kontroll(kumb):	#kumb võtab sisendiks kas 'Veerg' või 'Rida' 
+    while i < 8:
+    while i >=-8:
+        if seis[i+9][veerg] in vaenlased(värv):
+            käigud.append([i,veerg])
+            break
+        if seis[i+9][veerg] in omad(värv):
+            break
+        if seis[i+9][veerg] == '':
+            käigud.append([i,veerg])
+        i-=1'''
+def vankri_käigud(seis,vankri_pos,värv): 		#värvi on tegelikult võimalik leida ka nupu positsioonist(kas ta on väike või suur täht)
     rida=vankri_pos[0]
     veerg=vankri_pos[1]
     käigud=[]
@@ -49,17 +48,43 @@ vankri_käigud(seis,vankri_pos,värv) 		#värvi on tegelikult võimalik leida ka
             break
         if seis[i][veerg] in omad(värv):
             break
-        if seis[i][veerg] == '':
+        if seis[i][veerg] == ' ':
             käigud.append([i,veerg])
+    i=vankri_pos[0]-9
+    while i >= -8:
+        if seis[i][veerg] in vaenlased(värv):
+            käigud.append([i+8,veerg])
+            break
+        if seis[i][veerg] in omad(värv):
+            break
+        if seis[i][veerg] == ' ':
+            käigud.append([i+8,veerg])
+        i-=1
     for j in range(veerg+1,8):
         if seis[rida][j] in vaenlased(värv):
             käigud.append([rida,j])
             break
         if seis[rida][j] in omad(värv):
             break
-        if seis[rida][j] == '':
+        if seis[rida][j] == ' ':
             käigud.append([rida,j])
-    return käigud							#Hetkel peaks siia kaks samasugust for tsüklit veel tulema,
-print(vankri_käigud(algseis,[0,0],'must'))	#millega leiame ka ülesse ja vaskule jäävad võimalikud käigud
-
+    j=vankri_pos[1]-9
+    while j >= -8:
+        if seis[rida][j] in vaenlased(värv):
+            käigud.append([rida,j+8])
+            break
+        if seis[rida][j] in omad(värv):
+            break
+        if seis[rida][j] == ' ':
+            käigud.append([rida,j+8])
+        j-=1
+    return käigud
+def kuninga_käigud(seis,kuninga_pos):
+    if kuninga_pos.islower:
+        värv='M'
+    else:
+        värv='V'
+    
+print(vankri_käigud(algseis,[4,6],'M'))	
+#print(algseis[0][3-8])
         
